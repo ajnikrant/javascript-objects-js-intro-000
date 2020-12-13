c28380dbe9fe8088821c153742ea246ac1eddbcd
Amanda Nikrant <ajnikrant@gmail.com>
2020-12-13 22:28:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,13 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
+<<<<<<< HEAD
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
+=======
+function removeFromPlaylist(playlist, beatles, "Here comes the sun"){
+  delete playlist.beatles;
+
+  return playlist;
+>>>>>>> 733f9769949196a531c4d0e416e9e48c628083cd
 }
